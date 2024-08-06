9a7e4940a032d75679b7f6a8ed5084de5409f328
张长悦 <dancingcodes@qq.com>
2024-08-06 16:54:05 +0800

    1

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,8 @@ module.exports = (app) => {
     app.use(bodyParser.json());
 
     app.get('/', (req, res) => {
-        res.sendFile(path.join(process.cwd(), 'index.html'))
+        res.send('<h1 style="position: absolute;left: 50%;top: 40%;transform: translate(-50%, -50%);">Moonc Music Serve For Node.js</h1>')
     })
+
     app.use('/music', require('@/routes/music'));
 }
