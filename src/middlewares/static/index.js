891502629f15d7d5d7dcebdba83bd1237540455f
张长悦 <dancingcodes@qq.com>
2024-08-10 22:06:10 +0800

    1

--- src/middlewares/static/index.js
+++ src/middlewares/static/index.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-const { port } = require('@/config/serve')
+const { network } = require('@/config/serve')
 
 const uploadmusicPath = path.join(process.cwd(), 'src', 'public', 'upload', 'music');
-const baseUrl = process.env.NODE_ENV === 'development' ? `http://127.0.0.1:${port}` : 'https://music.moonc.love'
-const requestMusicPath = `${baseUrl}/music`
+const requestMusicPath = `${network}/music`
 
 const init = (app) => {
     fs.mkdirSync(uploadmusicPath, { recursive: true });
