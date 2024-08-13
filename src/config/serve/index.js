456fcd6c200a9763e9279e03f59f57ca580ff617
张长悦 <dancingcodes@qq.com>
2024-08-13 15:29:16 +0800

    1

--- src/config/serve/index.js
+++ src/config/serve/index.js
@@ -6,7 +6,6 @@ const path = require('path');
 const port = 3003
 const network = process.env.NODE_ENV === 'development' ? `http://127.0.0.1:${port}` : "https://musicapi.moonc.love"
 
-
 const init = (app) => {
     if (process.env.NODE_ENV === 'development') {
         app.listen(port, () => {
@@ -15,8 +14,8 @@ const init = (app) => {
         });
     } else {
         require('https').createServer({
-            key: fs.readFileSync(path.join(__filename, 'ssl', 'key.key')),
-            cert: fs.readFileSync(path.join(__filename, 'ssl', 'crt.crt'))
+            key: fs.readFileSync(path.join(__dirname, 'ssl', 'key.key')),
+            cert: fs.readFileSync(path.join(__dirname, 'ssl', 'crt.crt'))
         }, app).listen(port, () => {
             consoleLog.success(network)
             consoleLog.success(`Https服务已启动`)
