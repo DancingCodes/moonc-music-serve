f990b523fb8f4ca57fc475dab0dafc6a8c04817e
dancingcodes <dancingcodes@qq.com>
2024-09-18 15:20:32 +0800

    更新数据库

--- src/config/database/index.js
+++ src/config/database/index.js
@@ -3,7 +3,12 @@ const mongoose = require('mongoose');
 const consoleLog = require('@/utils/consoleLog')
 
 // 本地
-const path = 'mongodb://MooncMusic:123456@127.0.0.1:27017/MooncMusic?authSource=MooncMusic'
+const path = 'mongodb://MooncMusic:DancingCodes1227@127.0.0.1:27017/MooncMusic?authSource=MooncMusic'
+// db.createUser({
+//     user: "MooncMusic",
+//     pwd: "DancingCodes1227",
+//     roles: [{ role: "userAdmin", db: "MooncMusic" }]
+// })
 
 mongoose.connect(path).then(() => {
     consoleLog.success('数据库已连接')
